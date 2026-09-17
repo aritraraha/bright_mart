7b671a99469f8482ed914fe3b0d1e2bd6c5b5af1
Aritra Raha <p46159@irma.ac.in>
2026-09-17 19:20:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import joblib
 import numpy as np
 
 # Load the saved model
-model = pickle.load(open('linear_reg.sav', 'rb'))
+model = joblib.load(open('linear_reg.sav', 'rb'))
 
 st.title('Sales Prediction App')
 
